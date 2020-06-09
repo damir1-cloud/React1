ed2c91ce571e443a1966edec1db7c32fe08aea49
damir1-cloud <66561971+damir1-cloud@users.noreply.github.com>
2020-06-09 22:03:34 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
- </App name = "Danyal Amir" age = {23}>
+ <App name = "Danyal Amir" age = {23}/>
   
 
  ,
