15bb5cc21d171da9c4f83774fdf918c2e9c8ef0e
damir1-cloud <66561971+damir1-cloud@users.noreply.github.com>
2020-06-13 11:16:13 +0500

    toggle light color change added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import Hi from './test components/hello';
 import MediaCard from './test components/Mastercard';
-import Test from './Test';
+
 import Gate from './test components/Gate';
 
 let App = ({name}) => {
-
+let [IsLit, SetLit] = useState(true);
   return (
-    <div className = "maindiv">
+    <div className={`maindiv room ${IsLit? 'lit':'dark'}`}>
       <strong>This is my first react app and my name is {name} and my age is {23}</strong>
       <br/><br/>
 
@@ -16,7 +16,13 @@ let App = ({name}) => {
       <br/>
       <Gate/>
       <br/>
-      <Test/>
+      <div>
+           The room is {IsLit ? "Lit" : "Dark"}
+            <br/>
+            <button onClick = {()=> {
+                SetLit(!IsLit);
+            }}>Toggle light</button>
+        </div>
       <br/>
 
       <MediaCard title = "My name is Danyal" body = {<strong>this is the second assignment to test body para</strong>} ImageUrl = {"https://militarywatchmagazine.com/articles/2019/05/19/article_5ce11c405fffa9_11865733.png"}/>
